ef33d102e011ab059bbf7303f3669551e4d6378d
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2021-11-12 19:34:28 +0500

    Added Validation

--- index.js
+++ index.js
@@ -5,26 +5,33 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   if (req.url.includes("/webcrawler")) {
     let urlToCrawl = new URL("http://localhost:5000" + req.url);
-    let options = {
-      host: urlToCrawl.searchParams.get("url"),
-      path: "/",
-    };
+    let host = urlToCrawl.searchParams.get("url");
+    if (host) {
+      let options = {
+        host: host,
+        path: "/",
+      };
 
-    let request = http.request(options, (res) => {
-      let data = "";
-      res.on("data", (chunk) => {
-        data += chunk;
+      let request = http.request(options, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          console.log(data);
+        });
       });
 
-      res.on("end", () => {
-        console.log(data);
-      });
-    });
-
-    request.on("error", (err) => console.log(err.message));
-    request.end();
+      request.on("error", (err) => console.log(err.message));
+      request.end();
+      res.end("Website Crawled Successfully!");
+    }else{
+      res.end("Please also provide query parameters!")
+    }
+  } else {
+    res.end("Wrong Endpoint!");
   }
-  res.end("Website Crawled Successfully!");
 });
 
 server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
